bbc82b2593ac6eb596acef2f5ccdad669edfd135
Gilson da Gama <gilsondagama@hotmail.com>
2023-08-23 05:42:22 -0300

    Desafio RVP: Algumas validações com o Zod, Data com date-fns, mas de input com Reac-Inpu-Mask

--- src/formRPV/index.tsx
+++ src/formRPV/index.tsx
@@ -4,62 +4,74 @@ import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 
-import { useCallback, useEffect } from 'react'
-import { api } from '../server/axios'
+import { useCallback, useEffect, useState } from 'react'
+import { api } from '../service/axios'
 
-import MaskedInput from 'react-text-mask'
+import InputMask from 'react-input-mask'
+import { parseISO, subYears, isBefore } from 'date-fns'
 
-const schemaFormRVP = z
-  .object({
-    fullname: z.string().min(1, 'Nome é obrigatório').max(100),
-    // documentNumber: z
-    //   .string()
-    //   .min(11, 'CPF é obrigatório')
-    //   .max(14, 'CPF inválido'),
-    // birthdate: z
-    //   .string()
-    //   .min(9, 'Você precisa ter mais de 18 anos para continuar...'),
-    phoneNumber: z.string().min(11, 'contato é obrigatório').max(15),
-    address: z.object({
-      zipcode: z.string(),
-      // zipcode: z.string().refine((value) => value.length === 9, {
-      //   message: 'CEP deve conter 8 dígitos',
-      // }),
-      country: z.string(),
-      city: z.string(),
-      street: z.string(),
-      addressDistrict: z.string(),
-      addressNumber: z.string().min(1).max(40),
-      addressComplement: z.string().optional(),
+const schemaFormRVP = z.object({
+  fullname: z
+    .string()
+    .nonempty('Nome é obrigatório')
+    .max(100, 'Máximo 100 caracteres'),
+  documentNumber: z
+    .string()
+    .nonempty('O CPF é obrigatório')
+    .transform((value) => value.replace(/\D/g, '')) // Remove caracteres não numéricos
+    .refine((value) => value.length === 11, {
+      message: 'CPF deve conter 11 dígitos numéricos',
     }),
-    // email: z.string().email('email inválido'),
-    // minimumWage: z.number().positive(),
-    // educationLevel: z.string(),
-    // password: z
-    //   .string()
-    //   .nonempty('A senha é obrigatória')
-    //   .min(10, 'A senha deve ter no mínimo 10 caracteres'),
-    // confirmPassword: z.string(),
-  })
+  birthdate: z.string().refine(
+    (value) => {
+      const birthdayDate = parseISO(value)
+      const today = new Date()
+      const legalAgeDate = subYears(today, 18)
 
-  .transform((data) => ({
-    ...data,
-    address: {
-      ...data.address,
-      zipcode: data.address.zipcode.replace(/\D/g, ''),
+      return isBefore(birthdayDate, legalAgeDate)
     },
-  }))
+    {
+      message: 'Você deve ser maior de idade para continuar.',
+    },
+  ),
+  //   .string()
+  //   .min(9, 'Você precisa ter mais de 18 anos para continuar...'),
+  phoneNumber: z
+    .string()
+    .transform((value) => value.replace(/\D/g, '')) // Remove caracteres não numéricos
+    .refine((value) => value.length >= 10, {
+      message: 'Número de telefone deve conter pelo menos 10 dígitos numéricos',
+    }),
+  address: z.object({
+    zipcode: z
+      .string()
+      .transform((value) => value.replace(/\D/g, '')) // Remove caracteres não numéricos
+      .refine((value) => value.length === 8, {
+        message: 'CEP deve conter 8 dígitos',
+      }),
+    // country: z.string(),
+    // city: z.string(),
+    // street: z.string(),
+    // addressDistrict: z.string(),
+    // addressNumber: z.string(),
+    // addressComplement: z.string().optional(),
+  }),
+
+  // email: z.string().email('email inválido'),
+  // minimumWage: z.number().positive(),
+  // educationLevel: z.string(),
+  // password: z
+  //   .string()
+  //   .nonempty('A senha é obrigatória')
+  //   .min(10, 'A senha deve ter no mínimo 10 caracteres'),
+  // confirmPassword: z.string(),
+})
 
 // .transform((data) => ({
 //   ...data,
 //   address: {
-//     zipcode: data.address.zipcode,
-//     country: data.address.country,
-//     city: data.address.city,
-//     street: data.address.street,
-//     addressDistrict: data.address.addressDistrict,
-//     addressNumber: data.address.addressNumber,
-//     addressComplement: data.address.addressComplement,
+//     ...data.address,
+//     zipcode: data.address.zipcode.replace(/\D/g, ''),
 //   },
 // }))
 
@@ -74,6 +86,8 @@ type AddressProps = {
 }
 
 export function FormRPV() {
+  const [output, setOutput] = useState('')
+
   const {
     reset,
     register,
@@ -93,7 +107,11 @@ export function FormRPV() {
   const zipCode = watch('address.zipcode')
 
   const handleSubmitRVP = (data: FormPropsRVP) => {
+    // window.alert(JSON.stringify(data, null, 2))
+
+    setOutput(JSON.stringify(data, null, 2))
     console.log(data)
+
     reset()
   }
 
@@ -125,96 +143,102 @@ export function FormRPV() {
   }, [handleFetchAddress, zipCode, setValue])
 
   return (
-    <FormContainer onSubmit={handleSubmit(handleSubmitRVP)}>
-      {/* <FormInputDiv>
-        <label>Nome completo</label>
-        <input type="text" {...register('fullname')} />
-        <span>{errors.fullname ? errors.fullname.message : ' '}</span>
-      </FormInputDiv>
-       */}
+    <>
+      <FormContainer onSubmit={handleSubmit(handleSubmitRVP)}>
+        <FormInputDiv>
+          <label>Nome completo</label>
+          <input type="text" {...register('fullname')} />
+          <span>{errors.fullname ? errors.fullname.message : ' '}</span>
+        </FormInputDiv>
 
-      {/* 
-      <FormInputDiv>
-        <label>CPF</label>
-        <InputMask mask="999.999.999-99" {...register('documentNumber')} />
-        <span>
-          {errors.documentNumber ? errors.documentNumber.message : ' '}
-        </span>
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>CPF</label>
+          <InputMask mask="999.999.999-99" {...register('documentNumber')} />
+          <span>
+            {errors.documentNumber ? errors.documentNumber.message : ' '}
+          </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Data de Nascimento</label>
-        <InputMask mask="99/99/9999" {...register('birthdate')} />
-        <span>{errors.birthdate ? errors.birthdate.message : ' '}</span>
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Data de Nascimento</label>
+          <input type="date" {...register('birthdate')} />
+          <span>{errors.birthdate ? errors.birthdate.message : ' '}</span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Contato</label>
-        <InputMask mask="(99) 99999-9999" {...register('phoneNumber')} />
-        <span>{errors.phoneNumber ? errors.phoneNumber.message : ' '}</span>
-      </FormInputDiv> 
-      */}
+        <FormInputDiv>
+          <label>Contato</label>
+          <InputMask mask="(99) 9999-9999" {...register('phoneNumber')} />
+          <span>{errors.phoneNumber ? errors.phoneNumber.message : ' '}</span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>CEP</label>
-        <input
-          type="text"
-          // mask={[/\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/]}
-          {...register('address.zipcode')}
-        />
-        <span>
-          {errors.address?.zipcode ? errors.address?.zipcode.message : ' '}
-        </span>
-      </FormInputDiv>
+        <>
+          <FormInputDiv>
+            <label>CEP</label>
+            <InputMask mask="999-99999" {...register('address.zipcode')} />
+            <span>
+              {errors.address?.zipcode ? errors.address?.zipcode.message : ' '}
+            </span>
+          </FormInputDiv>
+          {/* 
 
-      <FormInputDiv>
-        <label>Estado</label>
-        <input type="text" disabled {...register('address.country')} />
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Estado</label>
+          <input type="text" disabled {...register('address.country')} />
+          <span> </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Cidade</label>
-        <input type="text" disabled {...register('address.city')} />
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Cidade</label>
+          <input type="text" disabled {...register('address.city')} />
+          <span> </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Endereço</label>
-        <input type="text" disabled {...register('address.street')} />
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Endereço</label>
+          <input type="text" disabled {...register('address.street')} />
+          <span> </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Bairro</label>
-        <input type="text" disabled {...register('address.addressDistrict')} />
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Bairro</label>
+          <input
+            type="text"
+            disabled
+            {...register('address.addressDistrict')}
+          />
+          <span> </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Número</label>
-        <input type="text" {...register('address.addressNumber')} />
-        <span>
-          {errors.address?.addressNumber
-            ? errors.address?.addressNumber.message
-            : ' '}
-        </span>
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Número</label>
+          <input type="text" {...register('address.addressNumber')} />
+          <span>
+            {errors.address?.addressNumber
+              ? errors.address?.addressNumber.message
+              : ' '}
+          </span>
+        </FormInputDiv>
 
-      <FormInputDiv>
-        <label>Complemento</label>
-        <input
-          type="text"
-          disabled
-          {...register('address.addressComplement')}
-        />
-      </FormInputDiv>
+        <FormInputDiv>
+          <label>Complemento</label>
+          <input
+            type="text"
+            disabled
+            {...register('address.addressComplement')}
+          />
+        </FormInputDiv> 
+        */}
+        </>
 
-      {/* 
+        {/*
       <FormInputDiv>
         <label>E-mail</label>
         <input type="email" {...register('email')} />
         <span>{errors.email ? errors.email.message : ' '}</span>
-      </FormInputDiv> 
+      </FormInputDiv>
       */}
 
-      {/* <FormInputDiv>
+        {/* <FormInputDiv>
         <label>Renda Mensal</label>
         <input
           type="number"
@@ -224,8 +248,8 @@ export function FormRPV() {
         <span>{errors.minimumWage ? errors.minimumWage.message : ' '}</span>
       </FormInputDiv> */}
 
-      {/* // educationLevel: string */}
-      {/* 
+        {/* // educationLevel: string */}
+        {/*
       <FormInputDiv>
         <label htmlFor="password">Senha</label>
         <input type="password" {...register('password')} />
@@ -238,10 +262,12 @@ export function FormRPV() {
         <span>
           {errors.confirmPassword ? errors.confirmPassword.message : ' '}
         </span>
-      </FormInputDiv> 
+      </FormInputDiv>
       */}
 
-      <button type="submit">Cadastrar</button>
-    </FormContainer>
+        <button type="submit">Cadastrar</button>
+      </FormContainer>
+      <pre>{output}</pre>
+    </>
   )
 }
